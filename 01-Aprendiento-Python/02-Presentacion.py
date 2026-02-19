1e984702fa1f759ce6604696f64719e9d3658392
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-02-18 21:35:53 -0400

    Estoy aprendiendo Python

---
+++ 01-Aprendiento-Python/02-Presentacion.py
@@ -0,0 +1,8 @@
+# Mostramos al usuario su nombre
+print("Nombre: Carlos Alberto")
+
+# Mostramos al usuario su edad
+print("Edad: 28")
+
+# Mostramos al usuario su pais de origen
+print("País: Santo Domingo")
