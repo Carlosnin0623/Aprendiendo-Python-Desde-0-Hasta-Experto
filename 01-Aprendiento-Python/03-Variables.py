ae3bf2ca9ac12f5251d9ed83b419303d82aa9bce
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-02-19 21:42:23 -0400

    Tipos de Variables en Python

---
+++ 01-Aprendiento-Python/03-Variables.py
@@ -0,0 +1,17 @@
+# Programa: Información personal
+
+# Guardar los datos en variables
+
+nombre = "Ana"
+edad = 22
+pais = 'México'
+frase_favorita = "Nunca dejes de aprender"
+
+# Mostramos la infomación en consola
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("País:", pais)
+print("Frase favorita:", frase_favorita)
+
+
+
---
+++ 01-Aprendiento-Python/04-Tipos_de_datos.py
@@ -0,0 +1,22 @@
+# 1. Enteros (int): Números exactos sin decimales
+vidas_jugador = 3
+puntos_totales = 1500
+
+# 2. Flotantes (float): Números con precisión decimal
+precio = 19.99
+pi = 3.1416
+temperatura = -5.5
+
+# ojo 10 es int, pero 10.0 es float
+
+# 3. Strings (str): Secuencias de caracteres 
+# Siempre van entre comillas (dobles o simples)
+
+curso = "Curso de Python"
+nivel = 'Principiante'
+
+# Esto es texto, No un número (como un código postal):
+codigo_postal = "28013"
+
+# print(codigo_postal + 5) -> Error!
+
