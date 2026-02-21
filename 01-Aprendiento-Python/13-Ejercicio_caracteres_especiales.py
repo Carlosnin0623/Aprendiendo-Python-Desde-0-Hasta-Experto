2a872ef13809e58506e8c93c983c7a14db042cf2
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-02-21 11:00:14 -0400

    Ejercicio Caracteres especiales

---
+++ 01-Aprendiento-Python/13-Ejercicio_caracteres_especiales.py
@@ -0,0 +1,21 @@
+# Programa: Ejemplo de caracteres especiales en Python
+
+# Salto de linea \n
+mensaje1 = "Hola\nMundo"
+print(mensaje1)
+
+# Tabulacion \t
+mensaje2 = "\tJuan"
+print(mensaje2)
+
+# Comillas dentro de cadena
+mensaje3 = "Ella dijo: \"Hola a todos\" "
+print(mensaje3)
+
+# Barra invertida \
+mensaje4 = "C:\\Usuarios\\Marce\\Documentos"
+print(mensaje4)
+
+# Cadenas cruda (raw string) r lleva un r delante y esto le dira a python que debe interpretar la cadena de forma literal
+mensaje5 = r"C:\Usuarios\Marce\Documentos"
+print(mensaje5)
