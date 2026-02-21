b4cf79de73bc9bd3f5c7a16b5de44c31efd3ac1a
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-02-20 21:29:20 -0400

    Creación de archivo cadena

---
+++ 01-Aprendiento-Python/10-Cadenas.py
@@ -0,0 +1,16 @@
+#1. Comillas simples (uso común)
+fruta = 'Manzana'
+
+#2. Comillas dobles (Funciona igual)
+
+#3. ¿Por que existen los dos? Para poner comillas dentro de texto
+# Sin complicarse con símbolos raros o extraños (\).
+
+#Ejemplo: Citar a alguien
+cita = 'El maestro dijo: "Estudien mucho"'
+
+#Ejemplo: Título de un libro
+libro = "Leí el libro 'El principito' ayer"
+
+print(cita) # Resultado: El maestro dijo: "Estudien mucho"
+print(libro) # Resultado: Leí el libro 'El principito' ayer
