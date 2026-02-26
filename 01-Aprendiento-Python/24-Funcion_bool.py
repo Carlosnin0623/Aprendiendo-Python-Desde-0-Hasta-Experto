a699c8489855e3715c1c8e2beaae880ee989b32a
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-02-26 17:01:19 -0400

    Funcion Bool

---
+++ 01-Aprendiento-Python/24-Funcion_bool.py
@@ -0,0 +1,21 @@
+"""
+La función bool(): Es el mecanismo de Python para determinar la "existencia" o el "Vacio" de un dato.
+
+Falsy:
+
+Representan "Nada" o "Vacio".
+
+0   (Cero int/float)
+" " (String Vacío)
+[] (Lista vacía)
+None (Ausencia de valor)
+
+Truthy
+
+Cualquier cosa que "Existe".
+
+1, -5, 3.14 (No cero)
+"Hola", " " (Espacio)
+[0] (Lista con datos)
+True
+"""
