4b0ed3639b2968b5ccb4f518d3d88d6b975e9893
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-03-05 11:37:13 -0400

    Actualizar Operadores de comparacion

--- 01-Aprendiento-Python/34-Operadores_comparacion.py
+++ 01-Aprendiento-Python/34-Operadores_comparacion.py
@@ -18,17 +18,49 @@ print(5 != 6) # True
 print(5 < 6) # True
 print(5 < 3) # False
 
-# Operador menor que (<=): Compara si el valor de la izquierda es menor y igual al de la derecha
+# Operador menor o igual que (<=): Compara si el valor de la izquierda es menor o igual al de la derecha
 
 print(5 <= 6) # True
 print(5 <= 3) # False
 
-# Operador menor que (>): Compara si el valor de la izquierda es mayor que el de la derecha
+# Operador mayor que (>): Compara si el valor de la izquierda es mayor que el de la derecha
 
 print(5 > 6) # False
 print(5 > 3) # True
 
-# Operador menor que (>=): Compara si el valor de la izquierda es mayor y igual al de la derecha
+# Operador mayor o igual que (>=): Compara si el valor de la izquierda es mayor o igual al de la derecha
 
-print(5 >= 6) # True
-print(5 >= 3) # False
+print(5 >= 6) # False
+print(5 >= 3) # True
+print(5 >= 5) # True
+
+
+print("*** Operadores Comparación (Relacionales) ***")
+
+"""
+Los operadores de comparación relacionales
+
+Los operadores de comparación relacionales se utilizan para comparar 2 valores, el resultado
+siempre es un valor booleano 'True' o 'False' dependiendo de que si la 
+condición se cumple o no.
+
+"""
+
+a,b = 5,5
+print(f'Valor inicial a: {a}, b {b}')
+
+# Operador igualdad == 
+resultado = (a == b)
+print(f'Resultado a == b: {resultado}')
+
+# Operador diferente !=
+resultado = (a != b)
+print(f'Resultado a != b: {resultado}')
+
+# Operador mayor que >
+resultado = a > b
+print(f'Resultado a > b: {resultado}')
+
+# Operador mayor o igual que >= 
+resultado = a >= b
+print(f'Resultado a >= b: {resultado}')
