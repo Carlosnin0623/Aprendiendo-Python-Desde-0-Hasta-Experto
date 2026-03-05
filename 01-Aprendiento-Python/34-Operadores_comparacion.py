1ec1dfef229724799b9ba02f84edad3e1907ac02
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-03-04 21:12:46 -0400

    Operadores de comparacion

---
+++ 01-Aprendiento-Python/34-Operadores_comparacion.py
@@ -0,0 +1,34 @@
+"""
+Los operadores de comparación se utilizan para comparar 2 valores, el resultado
+siempre es un valor booleano 'True' o 'False' dependiendo de que si la 
+condición se cumple o no.
+
+"""
+
+# Operador de igualdad (==): Compara si dos valores son iguales
+print(5 == 5) # True
+print(5 == 6) # False
+
+# Operador de distinto !=: Compara si dos valores son distintos.
+print(5 != 5) # False
+print(5 != 6) # True
+
+# Operador menor que (<): Compara si el valor de la izquierda es menor que el de la derecha
+
+print(5 < 6) # True
+print(5 < 3) # False
+
+# Operador menor que (<=): Compara si el valor de la izquierda es menor y igual al de la derecha
+
+print(5 <= 6) # True
+print(5 <= 3) # False
+
+# Operador menor que (>): Compara si el valor de la izquierda es mayor que el de la derecha
+
+print(5 > 6) # False
+print(5 > 3) # True
+
+# Operador menor que (>=): Compara si el valor de la izquierda es mayor y igual al de la derecha
+
+print(5 >= 6) # True
+print(5 >= 3) # False
